a663f0d8a29f61e0e900963179deafe687086bed
VanyaNeytrino <vanya.neytrino@yandex.ru>
2016-09-26 02:17:18 +0300

    finish

--- src/chat.cr
+++ src/chat.cr
@@ -1,9 +1,45 @@
-require "./chat/*"
 require "kemal"
 require "pg"
+require "./app/message"
 
-conn = PG.connect "postgres://chat:1488@localhost:5432/chat"
+conn = PG.connect("postgres://user:password@localhost:5432/db_name")
 
-module Chat
-  # TODO Put your code here
+sockets = [] of HTTP::WebSocket
+
+public_folder "src/assets"
+
+get "/" do
+  render "src/views/index.ecr"
 end
+
+ws "/" do |socket|
+  sockets.push socket
+
+  # Dispatch list of messages to the current socket
+
+  socket.send Message.all(conn).to_json
+
+  # Handle incoming message and dispatch it to all connected clients
+  socket.on_message do |message|
+    # Insert message into the Database
+    Message.from_json(message).insert(conn)
+
+    # Dispatch list of messages to all connected clients
+    sockets.each do |a_socket|
+      begin
+        a_socket.send Message.all(conn).to_json
+      rescue
+        sockets.delete(a_socket)
+        puts "Closing Socket: #{socket}"
+      end
+    end
+  end
+
+  # Handle disconnection and clean sockets
+  socket.on_close do |_|
+    sockets.delete(socket)
+    puts "Closing Socket: #{socket}"
+  end
+end
+
+Kemal.run
---
+++ src/db/init_db.cr
@@ -0,0 +1,29 @@
+
+require "pg"
+
+DB_NAME = "db_name"
+PG_PATH = "postgres://user:password@localhost:5432/db_name"
+
+# CREATES CONNECTION WITH DEFAULT POSTGRES conn
+conn = PG.connect(""postgres://user:password@localhost:5432/db_name")
+
+database_exists? = conn.exec(%{
+  SELECT CAST(1 AS integer)
+  FROM pg_database
+  WHERE datname=$1
+}, [DB_NAME]).to_hash.empty? ? false : true
+
+ puts "Creating messages table in #{DB_NAME}..."
+  conn.exec(%{
+    CREATE TABLE messages (
+      id          SERIAL PRIMARY KEY,
+      username    TEXT NOT NULL,
+      message     TEXT NOT NULL,
+      created_at  TIMESTAMP WITH TIME ZONE NOT NULL,
+      updated_at  TIMESTAMP WITH TIME ZONE NOT NULL
+    );
+  })
+  puts "Process finished succesfully"
+
+puts "Closing connection..."
+connB.close
